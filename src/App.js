41c20fe9a89d217fd575e3cdb8bf5ebae85def48
Kyle Knaggs <kyle@adfoam.com>
2019-05-28 20:13:16 -0400

    Initial commit from Kyle Knaggs

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>My first edit to my Create React App boiler plate file.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
