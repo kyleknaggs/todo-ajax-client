24fe39454dc1707b8a6eda82d9563560e499dd98
Kyle Knaggs <kyle@adfoam.com>
2019-07-21 23:05:12 -0400

    Add button hover styles.

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,17 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const StyledButton = styled.button`
-  background: ${props => props.type === "add" ? "green" : "red" };
+  background: ${props => props.type === "add" ? "#00C851" : "#FF4444" };
   border: none;
+  cursor: pointer;
   font-family: Permanent Marker;
   font-size: 32px;
   margin-bottom: 20px;
   margin-left: ${props => props.type === "add" ? "0px" : "20px" };
   padding: 5px 20px;
+  :hover{
+    background: ${props => props.type === "add" ? "#007E33" : "#CC0000" };
+  }
 `
 
 const Button = ({onClick, type}) => {
